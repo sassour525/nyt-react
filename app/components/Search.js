c0e410b544ad854112dee8e66744effee9cd1691
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-27 12:31:56 -0500

    added styles and form inputs

--- app/components/Search.js
+++ app/components/Search.js
@@ -6,12 +6,26 @@ var Search = React.createClass({
   render: function() {
     return (
     	<div className="search-container">
-				<div className="panel panel-default">
+				<div className="panel panel-default search-panel">
 					<div className="panel-heading">
 						<h3 className="panel-title">Search</h3>
 					</div>
 					<div className="panel-body">
-						<p><a className="btn btn-primary btn-lg search-button" href="#" role="button">Search</a></p>
+						<form>
+						  <div className="form-group">
+						    <label htmlFor="topic-input">Topic</label>
+						    <input type="text" className="form-control" id="topic-input" placeholder="Topic"/>
+						  </div>
+						  <div className="form-group">
+						    <label htmlFor="start-year-input">Start Year</label>
+						    <input type="text" className="form-control" id="start-year-input" placeholder="Start Year"/>
+						  </div>
+						  <div className="form-group">
+						    <label htmlFor="end-year-input">End Year</label>
+						    <input type="text" className="form-control" id="end-year-input" placeholder="End Year"/>
+						  </div>
+						  <p><a className="btn btn-primary btn-lg search-button" href="#" role="button">Search</a></p>
+						</form>
 					</div>
 				</div>
 				<div className="panel panel-default">
